0093ad522bd6e0c6fa57fb71227618e7f3316252
SureshKumar Kalanithi <s.kalanithi@wkelms.com>
2019-12-18 17:51:16 +0530

    all latest changes

--- test.js
+++ test.js
@@ -1,15 +1,11 @@
-
-window.onload=function(){
- 
-var main_test =document.querySelector('.btn-roll')
+var main_test = document.querySelector('.btn-roll')
 main_test.addEventListener('click', function() {
-  // if(gamePlaying) {
-      // 1. Random number
-      var random = Math.floor(Math.random() * 6) + 1;
+    // if(gamePlaying) {
+    // 1. Random number
+    var random = Math.floor(Math.random() * 6) + 1;
 
-      //2. Display the result
-      var diceDOM = document.getElementById('dice');
-      diceDOM.style.display = 'block';
-      diceDOM.src = 'dice-' + random + '.png';
-    });
-  }
+    //2. Display the result
+    var diceDOM = document.getElementById('dice');
+    // diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + random + '.png';
+});
